f1897f2c6cbe1cc4224294613515c274358e8c1b
treasureLxx <1073718211@qq.com>
2022-04-16 15:32:02 +0800

    批量换肤

--- source/components/player.py
+++ source/components/player.py
@@ -1,18 +1,29 @@
 import pygame
 from .. import tools, setup
 from .. import constants as C
+import json
+import os
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, name):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
+        self.load_data()
         self.setup_states()
         self.setup_velocities()
         self.setup_timers()
         self.load_images()
 
+    def load_data(self):
+        file_name = self.name + '.json'
+        file_path = os.path.join('source/data/player', file_name)
+        with open(file_path) as f:
+            self.player_data = json.load(f)
+
+
     def setup_states(self):
+        self.state = 'walk'
         self.face_right = True
         self.dead = False
         self.big = False
@@ -27,27 +38,45 @@ class Player(pygame.sprite.Sprite):
 
     def load_images(self):
         sheet = setup.GRAPHICS['mario_bros']
-        self.right_frames = []
-        self.left_frames = []
-        self.up_frames = []
-        self.down_frames = []
+        frame_rects = self.player_data['image_frames']
 
-        frame_rects = [
-            (178, 32, 12, 16),
-            (80, 32, 15, 16),
-            (96, 32, 16, 16),
-            (112, 32, 16, 16)
+        self.right_small_normal_frames = []
+        self.right_big_normal_frames = []
+        self.right_big_fire_frames = []
+        self.left_small_normal_frames = []
+        self.left_big_normal_frames = []
+        self.left_big_fire_frames = []
+
+        self.small_normal_frames = [self.right_small_normal_frames, self.left_small_normal_frames]
+        self.big_normal_frames = [self.right_big_normal_frames, self.left_big_normal_frames]
+        self.big_fire_frames = [self.right_big_fire_frames, self.left_big_fire_frames]
+
+        self.all_frames = [
+            self.right_small_normal_frames,
+            self.right_big_normal_frames,
+            self.right_big_fire_frames,
+            self.left_small_normal_frames,
+            self.left_big_normal_frames,
+            self.left_big_fire_frames,
         ]
 
-        for frame_rect in frame_rects:
-            right_image = tools.get_image(sheet, *frame_rect, (0, 0, 0), C.PLAYER_MULTI)
-            left_image = pygame.transform.flip(right_image, True, False)
-            up_right = pygame.transform.rotate(right_image, 90)
-            down_image = pygame.transform.rotate(right_image, -90)
-            self.right_frames.append(right_image)
-            self.left_frames.append(left_image)
-            self.up_frames.append(up_right)
-            self.down_frames.append(down_image)
+        self.right_frames = self.right_small_normal_frames
+        self.left_frames = self.left_small_normal_frames
+
+        for group, group_frame_rects in frame_rects.items():
+            for frame_rect in group_frame_rects:
+                right_image = tools.get_image(sheet, frame_rect['x'], frame_rect['y'], frame_rect['width'], frame_rect['height'],
+                                              (0, 0, 0), C.PLAYER_MULTI)
+                left_image = pygame.transform.flip(right_image, True, False)
+                if group == 'right_small_normal':
+                    self.right_small_normal_frames.append(right_image)
+                    self.left_small_normal_frames.append(left_image)
+                if group == 'right_big_normal':
+                    self.right_big_normal_frames.append(right_image)
+                    self.left_big_normal_frames.append(left_image)
+                if group == 'right_big_fire':
+                    self.right_big_fire_frames.append(right_image)
+                    self.left_big_fire_frames.append(left_image)
 
         self.frame_index = 0
         self.frames = self.right_frames
@@ -57,25 +86,25 @@ class Player(pygame.sprite.Sprite):
     def update(self, keys):
         self.current_time = pygame.time.get_ticks()
         if keys[pygame.K_RIGHT]:
+            self.state = 'walk'
             self.x_vel = 5
             self.y_vel = 0
             self.frames = self.right_frames
         if keys[pygame.K_LEFT]:
+            self.state = 'walk'
             self.x_vel = -5
             self.y_vel = 0
             self.frames = self.left_frames
-        if keys[pygame.K_UP]:
-            self.x_vel = 0
-            self.y_vel = -5
-            self.frames = self.up_frames
-        if keys[pygame.K_DOWN]:
-            self.x_vel = 0
+        if keys[pygame.K_SPACE]:
+            self.state = 'iump'
             self.y_vel = 5
-            self.frames = self.down_frames
-        if self.current_time - self.walking_timer > 100:
-            self.walking_timer = self.current_time
-            self.frame_index += 1
-            self.frame_index %= 4
+        if self.state == 'walk':
+            if self.current_time - self.walking_timer > 100:
+                self.walking_timer = self.current_time
+                self.frame_index += 1
+                self.frame_index %= 4
+        if self.state == 'jump':
+            self.frame_index = 4
         self.image = self.frames[self.frame_index]
 
 
